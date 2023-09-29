becc43322887d83b43dc0d6004102f07566cddf6
Gabriel Coronel <gabriel.c1901@gmail.com>
2023-09-29 20:05:13 -0300

    add jwt authentication with redux

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,15 @@
+import { apiSlice } from "../../app/api/apiSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/api/v1/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApiSlice;
